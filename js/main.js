cde557eeefb38f5664940daa657daa799e39a8e6
Gabriel <gabrielulu@gmail.com>
2017-11-09 09:40:00 +0800

    Barcode scanner with maximum frequency code output

---
+++ js/main.js
@@ -0,0 +1,78 @@
+var results = {}
+var video = $("#gum-local")[0]
+
+var constraints = { audio: false, video: true }
+
+navigator.mediaDevices.getUserMedia(constraints)
+
+.then(function (stream) {
+  console.log('activated', stream)
+  video.srcObject = stream
+})
+
+.catch(function (err) {
+  console.log(err)
+})
+
+Quagga.init({
+    inputStream : {
+      name : "Live",
+      type : "LiveStream",
+      target: '#gum-local'    // Or '#yourElement' (optional)
+    },
+    decoder : {
+      // readers : ['ean_reader','ean_8_reader','code_39_reader','code_39_vin_reader','codabar_reader','upc_reader','upc_e_reader','code_128_reader'],
+      readers : ['ean_reader'],
+      config: {}
+    }
+  }, function(err) {
+      if (err) {
+          console.log(err);
+          return
+      }
+      console.log("Initialization finished. Ready to start");
+      Quagga.start();
+  })
+
+  Quagga.onProcessed(function(result) {
+        var drawingCtx = Quagga.canvas.ctx.overlay,
+            drawingCanvas = Quagga.canvas.dom.overlay;
+
+        if (result) {
+            if (result.boxes) {
+                drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
+                result.boxes.filter(function (box) {
+                    return box !== result.box;
+                }).forEach(function (box) {
+                    Quagga.ImageDebug.drawPath(box, {x: 0, y: 1}, drawingCtx, {color: "green", lineWidth: 2});
+                });
+            }
+
+            if (result.box) {
+                Quagga.ImageDebug.drawPath(result.box, {x: 0, y: 1}, drawingCtx, {color: "#00F", lineWidth: 2});
+            }
+
+            if (result.codeResult && result.codeResult.code) {
+                Quagga.ImageDebug.drawPath(result.line, {x: 'x', y: 'y'}, drawingCtx, {color: 'red', lineWidth: 3});
+            }
+        }
+    })
+
+    Quagga.onDetected(function(result) {
+        var code1 = result.codeResult.code;
+        if (results[code1] === undefined) results[code1] = 1
+        else results[code1] += 1
+
+          console.log(results)
+          var frequency = Object.values(results)
+          var totalScans = frequency.reduce(function(a, b) {
+            return a + b
+          })
+          // console.log(totalScans)
+          // console.log('freq', frequency)
+          var maxFrequency = Math.max.apply(null, frequency)
+          // console.log('maxFrequency', maxFrequency)
+          // if (maxFrequency > (0.7 * totalScans) && totalScans > 20) {
+          console.log('max',Object.keys(results).find(key => results[key] === maxFrequency))
+        // }
+    });
